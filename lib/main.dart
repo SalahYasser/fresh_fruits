f5cfbf150488b5e34481b3321f052208f5262a19
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-08 11:38:35 +0200

    Add and implemented on Generate Routes function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'core/helper_functions/on_generate_routes.dart';
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -12,7 +13,9 @@ class FruitsHub extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashView(),
+      // home: SplashView(),
+      onGenerateRoute: onGenerateRoutes,
+      initialRoute: SplashView.routeName,
     );
   }
 }
