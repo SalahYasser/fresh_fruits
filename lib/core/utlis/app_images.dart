ca03e23e01886914d9bf5df3329d715e6ef1c93b
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-08 12:39:11 +0200

    implement splash view body

---
+++ lib/core/utlis/app_images.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for assetsImagesPlant
+  /// assets/images/plant.svg
+  static const String imagesPlant = "assets/images/plant.svg";
+
+  /// Assets for assetsImagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+}
