2a3dda813bc2fc4102edfc45b5ec546ba405c3ee
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-08 17:08:31 +0200

    create page view item

--- lib/core/utlis/app_images.dart
+++ lib/core/utlis/app_images.dart
@@ -1,20 +1,37 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
   /// Assets for assetsImagesAppIcon
   /// assets/images/app_icon.png
-  static const String imagesAppIcon = "assets/images/app_icon.png";
+  static const String assetsImagesAppIcon = "assets/images/app_icon.png";
 
   /// Assets for assetsImagesLogo
   /// assets/images/logo.svg
-  static const String imagesLogo = "assets/images/logo.svg";
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+
+  /// Assets for assetsImagesPageViewItem1BgImage
+  /// assets/images/page_view_item1_bg_image.svg
+  static const String assetsImagesPageViewItem1BgImage = "assets/images/page_view_item1_bg_image.svg";
+
+  /// Assets for assetsImagesPageViewItem1Image
+  /// assets/images/page_view_item1_image.svg
+  static const String assetsImagesPageViewItem1Image = "assets/images/page_view_item1_image.svg";
+
+  /// Assets for assetsImagesPageViewItem2BgImage
+  /// assets/images/page_view_item2_bg_image.svg
+  static const String assetsImagesPageViewItem2BgImage = "assets/images/page_view_item2_bg_image.svg";
+
+  /// Assets for assetsImagesPageViewItem2Image
+  /// assets/images/page_view_item2_image.svg
+  static const String assetsImagesPageViewItem2Image = "assets/images/page_view_item2_image.svg";
 
   /// Assets for assetsImagesPlant
   /// assets/images/plant.svg
-  static const String imagesPlant = "assets/images/plant.svg";
+  static const String assetsImagesPlant = "assets/images/plant.svg";
 
   /// Assets for assetsImagesSplashBottom
   /// assets/images/splash_bottom.svg
-  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+  static const String assetsImagesSplashBottom = "assets/images/splash_bottom.svg";
 }
+
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utlis/app_images.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
   const OnBoardingPageView({super.key});
@@ -6,7 +8,21 @@ class OnBoardingPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: const [],
+      children: const [
+        PageViewItem(
+          image: Assets.assetsImagesPageViewItem1Image,
+          backgroundImage: Assets.assetsImagesPageViewItem1BgImage,
+          subtitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            children: [
+              Text('مرحبًا بك في'),
+              Text('Fruits'),
+              Text('HUB'),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
