53104f30fd64cf2bf3516ea206d31494c43f400d
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-14 14:46:07 +0200

    add have an account widget to signup view body

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/terms_and_conditions.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/have_an_account.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
@@ -31,6 +33,13 @@ class SignupViewBody extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             const TermsAndConditions(),
+            const SizedBox(height: 30),
+            CustomButton(
+              onPressed: () {},
+              text: 'إنشاء حساب جديد',
+            ),
+            const SizedBox(height: 24),
+            const HaveAnAccount()
           ],
         ),
       ),
