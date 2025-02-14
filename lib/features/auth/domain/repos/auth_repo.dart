155135c0a63cc2e2826933a9e28a0da77e357aa7
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-14 16:44:26 +0200

    add either to auth repo

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/failures.dart';
 import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
 
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(
     String email,
     String password,
   );
