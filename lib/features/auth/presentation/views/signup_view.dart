f4e02f62e7a82203bbb46ac1ab4b93e4aa57aebb
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-14 22:39:46 +0200

    create signup view body bloc consumer and integrate signup cubit

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -4,7 +4,7 @@ import 'package:fruits_hub/core/helper_functions/build_app_bar.dart';
 import 'package:fruits_hub/core/services/git_it_service.dart';
 import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubit.dart';
-import 'package:fruits_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart';
 
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
@@ -19,7 +19,7 @@ class SignupView extends StatelessWidget {
       ),
       child: Scaffold(
         appBar: buildAppBar(context, title: 'حساب جديد'),
-        body: SignupViewBody(),
+        body: SignupViewBodyBlocConsumer(),
       ),
     );
   }
