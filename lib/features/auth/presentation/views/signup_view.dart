91364a0b167bb21235421337b29d73f9b98b86e6
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-14 18:58:58 +0200

    create git it service and provide signup cubit

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/helper_functions/build_app_bar.dart';
+import 'package:fruits_hub/core/services/git_it_service.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -11,7 +15,12 @@ class SignupView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: SignupViewBody(),
+      body: BlocProvider(
+        create: (context) => SignupCubit(
+          getIt<AuthRepo>(),
+        ),
+        child: SignupViewBody(),
+      ),
     );
   }
 }
