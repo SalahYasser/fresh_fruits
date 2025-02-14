ec1718488f252819803ced9f6652fcdd33df9050
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-14 16:38:26 +0200

    create user Entity

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,9 @@
+import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+
+  Future<UserEntity> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+}
