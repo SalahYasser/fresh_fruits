aacd5f657a5782fe7b8570edd6ca6996744aecd4
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-15 11:52:07 +0200

    hide the password and allow to user switch between visible and non-visible password

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -8,16 +8,19 @@ class CustomTextFormField extends StatelessWidget {
     required this.hintText,
     this.suffixIcon,
     this.onSaved,
+    this.obscureText = false,
   });
 
   final TextInputType textInputType;
   final String hintText;
   final Widget? suffixIcon;
   final void Function(String?)? onSaved;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: obscureText,
       onSaved: onSaved,
       validator: (value) {
         if (value == null || value.isEmpty) {
--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_hub/core/widgets/password_field.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/terms_and_conditions.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/have_an_account.dart';
@@ -48,13 +49,10 @@ class _SignupViewBodyState extends State<SignupViewBody> {
                 hintText: 'البريد الإلكتروني',
               ),
               const SizedBox(height: 16),
-              CustomTextFormField(
+              PasswordField(
                 onSaved: (value) {
                   password = value!;
                 },
-                textInputType: TextInputType.visiblePassword,
-                hintText: 'كلمة المرور',
-                suffixIcon: Icon(Icons.remove_red_eye),
               ),
               const SizedBox(height: 16),
               const TermsAndConditions(),
@@ -83,3 +81,4 @@ class _SignupViewBodyState extends State<SignupViewBody> {
     );
   }
 }
+
