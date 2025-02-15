0623bfc3bf0f2b76cb714285438649fd24d08df4
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-15 21:23:03 +0200

    integrate sign in cubit

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -4,7 +4,7 @@ import 'package:fruits_hub/core/helper_functions/build_app_bar.dart';
 import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/singin_cubit/singin_cubit.dart';
-import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
@@ -14,12 +14,13 @@ class SigninView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => SigninCubit(
-        getIt.get<AuthRepo>(),
-      ),
+      create: (context) =>
+          SigninCubit(
+            getIt.get<AuthRepo>(),
+          ),
       child: Scaffold(
         appBar: buildAppBar(context, title: 'تسجيل الدخول'),
-        body: const SigninViewBody(),
+        body: const SigninViewBodyBlocConsumer(),
       ),
     );
   }
---
+++ lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/widgets/custom_progress_hud.dart';
+import 'package:fruits_hub/features/auth/presentation/cubits/singin_cubit/singin_cubit.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+
+class SigninViewBodyBlocConsumer extends StatelessWidget {
+  const SigninViewBodyBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SigninCubit, SigninState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return CustomProgressHud(
+          isLoading: state is SigninLoading ? true : false,
+          child: SigninViewBody(),
+        );
+      },
+    );
+  }
+}
