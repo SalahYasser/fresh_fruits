cf43a8fdabb0da70f336ffeb7ce2effaedb8df50
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-17 13:45:43 +0200

    create add user data method

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -19,4 +19,6 @@ abstract class AuthRepo {
   Future<Either<Failure, UserEntity>> signInWithFacebook();
 
   Future<Either<Failure, UserEntity>> signInWithApple();
+
+  Future addUserDate({required UserEntity user});
 }
