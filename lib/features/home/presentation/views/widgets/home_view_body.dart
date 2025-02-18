650e92e173b244f25e9cf7e252e0152cc6b6ec14
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-18 14:04:58 +0200

    create best selling header

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/widgets/search_text_field.dart';
+import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_header.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/featured_list.dart';
 
@@ -22,6 +23,8 @@ class HomeViewBody extends StatelessWidget {
                 CustomTextField(),
                 SizedBox(height: kTopPadding),
                 FeaturedList(),
+                SizedBox(height: kTopPadding),
+                BestSellingHeader(),
               ],
             ),
           ),
