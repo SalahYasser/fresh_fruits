2641970a1c63e8d1da7be5cd8770ec8e6eaca7ef
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-22 16:51:31 +0200

    provide and trigger products cubit

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,9 +1,12 @@
+import 'package:fruits_hub/core/repos/products_repo/products_repo.dart';
 import 'package:fruits_hub/core/services/database_service.dart';
 import 'package:fruits_hub/core/services/firebase_auth_service.dart';
 import 'package:fruits_hub/core/services/firestore_service.dart';
 import 'package:fruits_hub/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:get_it/get_it.dart';
+
+import '../repos/products_repo/products_repo_impl.dart';
 
 final getIt = GetIt.instance;
 
@@ -17,6 +20,12 @@ void setupGetIt() {
     AuthRepoImpl(
       firebaseAuthService: getIt<FirebaseAuthService>(),
       dataBaseService: getIt<DataBaseService>(),
+    ),
+  );
+
+  getIt.registerSingleton<ProductsRepo>(
+    ProductsRepoImpl(
+      getIt<DataBaseService>(),
     ),
   );
 }
