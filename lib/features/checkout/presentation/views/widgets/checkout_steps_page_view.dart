bcfdb4ab80253f221f0c9ce5dad99954184d226e
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-26 21:08:03 +0200

    Create checkout page view

---
+++ lib/features/checkout/presentation/views/widgets/checkout_steps_page_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'checkout_steps.dart';
+
+class CheckoutStepsPageView extends StatelessWidget {
+  const CheckoutStepsPageView({super.key, required this.pageController});
+
+  final PageController pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: pageController,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: getSteps().length,
+      itemBuilder: (context, index) {
+        return SizedBox();
+      },
+    );
+  }
+}
--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 
 import '../../../../../constants.dart';
 import 'checkout_steps.dart';
+import 'checkout_steps_page_view.dart';
 
-class CheckoutViewBody extends StatelessWidget {
+class CheckoutViewBody extends StatefulWidget {
   const CheckoutViewBody({super.key});
+
+  @override
+  State<CheckoutViewBody> createState() => _CheckoutViewBodyState();
+}
+
+class _CheckoutViewBodyState extends State<CheckoutViewBody> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +33,16 @@ class CheckoutViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
       child: Column(
         children: [
-          SizedBox(height: 20),
-          CheckoutSteps(),
+          const SizedBox(height: 20),
+          const CheckoutSteps(),
+          Expanded(
+            child: CheckoutStepsPageView(pageController: pageController),
+          ),
+          CustomButton(
+            text: 'التالي',
+            onPressed: () {},
+          ),
+          SizedBox(height: 32),
         ],
       ),
     );
