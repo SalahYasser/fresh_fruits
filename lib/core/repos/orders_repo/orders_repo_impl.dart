90501e9a9524ae0b898d250674b83ae14ac46464
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-05-17 19:41:21 +0300

    checkout feature done

--- lib/core/repos/orders_repo/orders_repo_impl.dart
+++ lib/core/repos/orders_repo/orders_repo_impl.dart
@@ -6,26 +6,6 @@ import '../../../features/checkout/data/models/order_input_model.dart';
 import '../../utils/backend_endpoint.dart';
 import 'orders_repo.dart';
 
-// class OrdersRepoImpl implements OrdersRepo {
-//   final DataBaseService dataBaseService;
-//
-//   OrdersRepoImpl(this.dataBaseService);
-//
-//   @override
-//   Future<Either<Failure, void>> addOrder(
-//       {required OrderInputEntity order}) async {
-//     try {
-//       await dataBaseService.addData(
-//         path: BackendEndpoint.addOrder,
-//         data: OrderInputModel.fromEntity(order).toJson(),
-//       );
-//       return right(null);
-//     } catch (e) {
-//       return left(ServerFailure(e.toString()));
-//     }
-//   }
-// }
-
 class OrdersRepoImpl implements OrdersRepo {
   final DataBaseService dataBaseService;
 
@@ -38,7 +18,7 @@ class OrdersRepoImpl implements OrdersRepo {
       var orderModel = OrderModel.fromEntity(order);
       await dataBaseService.addData(
         path: BackendEndpoint.addOrder,
-        // documentId: orderModel.uId, //TODO : documentId send only one edit document
+        documentId: orderModel.uId,
         data: orderModel.toJson(),
       );
       return const Right(null);
